00fe47ed3a7acf09a90157f270115033fb79e116
LiterallyParker <parkernash2001@gmail.com>
2024-06-11 19:12:51 -0500

    commit 1

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ContactRow from "./ContactRow";
 
 const dummyContacts = [
@@ -6,8 +6,22 @@ const dummyContacts = [
   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
 ];
+
 export default function ContactList() {
  const [contacts, setContacts] = useState(dummyContacts);
+
+ useEffect(() => {
+  async function fetchContacts() {
+    try {
+      const response = await fetch("https://jsonplace-univclone.herokuapp.com/users");
+      const result = await response.json();
+      return result
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchContacts();
+}, []);
 
   return (
     <table>
